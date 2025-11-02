666c47ddf2a784d4f9f5aadfa1fe9d88377f7fa8
Jean <jeanikkt@gmail.com>
2025-11-02 03:26:18 -0300

    feat: Forçar uso de HTTPS em produção no AppServiceProvider e desativar força HTTPS no fly.toml

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Força o uso de HTTPS em produção
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        // Mantém o prefetch do Vite
         Vite::prefetch(concurrency: 3);
     }
 }
